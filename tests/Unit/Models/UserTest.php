8ec297ff2b349c6838929e0bb1b3f293d51fc2a3
AyrielNoriega <anoriega2310@gmail.com>
2023-05-10 19:25:56 -0500

    config first test

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -3,14 +3,18 @@
 namespace Tests\Unit\Models;
 
 use PHPUnit\Framework\TestCase;
+use Illuminate\Database\Eloquent\Collection;
+use App\Models\User;
 
 class UserTest extends TestCase
 {
     /**
      * A basic unit test example.
      */
-    public function test_example(): void
+    public function test_has_many_repositories(): void
     {
-        $this->assertTrue(true);
+        $user = new User;
+
+        $this->assetInstanceOf(Collection::class, $user->repositories);
     }
 }
